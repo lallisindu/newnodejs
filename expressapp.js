7ada6c56faeffe7fd9e62016ed4eef334250a186
lallisindu <lallisindu123@gmail.com>
2023-12-23 14:39:36 +0530

    add produt routes and product page redirecting

--- expressapp.js
+++ expressapp.js
@@ -1,13 +1,20 @@
-const http=require('http');
+//const http=require('http');
 
 const express=require('express');
+const bodyParser= require('body-parser');
 const app=express();
-app.use((req,res,next) => {
-    console.log('in the meeting')
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/add-prod',(req,res,next) => {
+    res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><input type="text" name="title"><button type="submit">add product</button></form></body></html>');
+    
 });
-app.use((req,res,next) => {
-    console.log('in the another meeting')
+app.use('/product',(req,res,next) => {
+    console.log(req.body);
+    res.redirect('/')
+   // res.send('<h1>Hellow from express</h1>');
+});
+app.use('/',(req,res,next) => {
+    //console.log('in the another meeting')
     res.send('<h1>Hellow from express</h1>');
 });
 //const server = http.createServer(app);
